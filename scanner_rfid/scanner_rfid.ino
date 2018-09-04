deaf5d75bf2272a0615fdc693d168bdfc28b79f3
Jérémy Bourel <calaruni@hotmail.com>
2018-09-04 20:38:18 +0200

    Init project

---
+++ scanner_rfid/scanner_rfid.ino
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);   // Initialize serial communications with the PC
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Veuillez scanner votre carte."));
+}
+
+void loop() {
+  // Look for new cards
+  if ( !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
