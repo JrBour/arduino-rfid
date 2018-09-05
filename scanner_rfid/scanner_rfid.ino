da444101a4fefb74b74478c94ea21500a2008a5a
Jérémy Bourel <calaruni@hotmail.com>
2018-09-05 10:42:12 +0200

    edit project : display errors on monitor

--- scanner_rfid/scanner_rfid.ino
+++ scanner_rfid/scanner_rfid.ino
@@ -12,23 +12,24 @@ void setup() {
   Serial.begin(9600);   // Initialize serial communications with the PC
   SPI.begin();      // Init SPI bus
   mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   pinMode(pinLEDVerte, OUTPUT);
   pinMode(pinLEDRouge, OUTPUT);
   Serial.println(F("Veuillez scanner votre carte.")); // Function F(), allow to store string in flash memory instead of the RAM
 }
 
-void checkCard(){
+void checkCard() {
   // Check if a card is present
   if ( !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
     return;
- 
+
   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-  
+
   if (mfrc522.uid.uidByte[0] == 0xA0 && mfrc522.uid.uidByte[1] == 0xD4 && mfrc522.uid.uidByte[2] == 0xE1 && mfrc522.uid.uidByte[3] == 0x2B) {
-     Serial.println("Accés autorisé");
-     digitalWrite(LED_GREEN, HIGH);
-     delay(3000);
-     digitalWrite(LED_GREEN, LOW);
+ d   Serial.println("Accés autorisé");
+    digitalWrite(LED_GREEN, HIGH);
+    delay(3000);
+    digitalWrite(LED_GREEN, LOW);
   } else {
     Serial.println("Accés refusé");
     digitalWrite(LED_RED, HIGH);
