c8cad1a19c6f0abae4d0582e0242fded3435b014
Jérémy Bourel <calaruni@hotmail.com>
2018-09-05 10:30:11 +0200

    edit project : add condition for authentification

--- scanner_rfid/scanner_rfid.ino
+++ scanner_rfid/scanner_rfid.ino
@@ -1,25 +1,32 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_PIN          10         // Configurable, see typical pin layout above
+#define RST_PIN  9          // Configurable, see typical pin layout above
+#define SS_PIN   10         // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
   Serial.begin(9600);   // Initialize serial communications with the PC
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();      // Init SPI bus
   mfrc522.PCD_Init();   // Init MFRC522
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Veuillez scanner votre carte."));
+  Serial.println(F("Veuillez scanner votre carte.")); // Function F(), allow to store string in flash memory instead of the RAM
+}
+
+void checkCard(){
+  // Check if a card is present
+  if ( !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
+    return;
+  
+
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  if (mfrc522.uid.uidByte[0] == 0xA0 && mfrc522.uid.uidByte[1] == 0xD4 && mfrc522.uid.uidByte[2] == 0xE1 && mfrc522.uid.uidByte[3] == 0x2B) {
+     Serial.println("Accés autorisé");
+  } else {
+    Serial.println("Accés refusé");
+  }
 }
 
 void loop() {
-  // Look for new cards
-  if ( !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  checkCard();
 }
